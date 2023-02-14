6d3811a21094818801a02f977450442ce5cf9b2a
unknown <joshuaretiza96@gmail.com>
2023-02-14 15:14:42 +0800

    first commit

---
+++ src/components/ChartComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <h1>Chart</h1>
+    <div>
+        <canvas id="myChart"></canvas>
+    </div>
+</template>
+
+<script setup>
+import Chart from 'chart.js/auto';
+
+</script>
+
+<script>
+export default {
+    mounted() {
+        const ctx = document.getElementById('myChart');
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="2">
+        <NavBar />
+      </v-col>
+      <v-col cols="10">
+        <ChartComponent />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+import ChartComponent from '@/components/ChartComponent.vue'
+import NavBar from '@/components/NavBar.vue';
+</script>
